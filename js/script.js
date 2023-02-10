ba18abea01ea44b417f2b5f3413b2a97a6ba78d0
Gustavo Galparoli <tavog_5@hotmail.com>
2023-02-10 19:43:26 -0300

    actualizando links

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var typingEffect = new  Typed(".multiText",{
-    strings : ["coder" , "developer"],
+    strings : ["youtuber", "developer" ,"coder"  ],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
-    backDelay : 1500,
+    backDelay : 2000,
 })
